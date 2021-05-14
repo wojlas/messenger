74c93ddf1174adf76bd0e450e4ad61b046781afe
21cukiel <wojlas93@gmail.com>
2021-05-14 17:32:47 +0200

    Program create_db makes db and tables

--- create_db.py
+++ create_db.py
@@ -1,7 +1,11 @@
 from psycopg2 import connect, OperationalError
-from psycopg2.errors import DuplicateDatabase
+from psycopg2.errors import DuplicateDatabase, DuplicateTable
+
+"""Program makes database (db name input by user) and tables for users and messages"""
 
 db_name = input("Name your database: ")
+user_table_name = 'users'
+messages_table_name = 'messages'
 create_db = f"create database {db_name};"
 
 create_users = """
@@ -23,13 +27,14 @@ create_messages = """
     foreign key(to_id) references  users on delete cascade
     );"""
 
+#login data
 db_user = "postgres"
 db_password = "coderslab"
 host = "127.0.0.1"
 
-
+#create database
 try:
-    cnx = connect(user = db_user, password = db_password, host = host)
+    cnx = connect(user=db_user, password=db_password, host=host)
     cnx.autocommit = True
     cursor = cnx.cursor()
     cursor.execute(create_db)
@@ -37,7 +42,33 @@ except OperationalError as e:
     print("ERROR!", e)
 except DuplicateDatabase:
     print(f"Database {db_name} already exist.")
+    pass
 
+#create table users in database
+try:
+    cnx = connect(user=db_user, password=db_password, host=host, database = db_name)
+    cnx.autocommit = True
+    cursor = cnx.cursor()
+    cursor.execute(create_users)
+except OperationalError as e:
+    print("ERROR!", e)
+except DuplicateTable:
+    print(f"Table {user_table_name} already exist.")
+    pass
+
+#create table messages in database
+try:
+    cnx = connect(user=db_user, password=db_password, host=host, database = db_name)
+    cnx.autocommit = True
+    cursor = cnx.cursor()
+    cursor.execute(create_messages)
+except OperationalError as e:
+    print("ERROR!", e)
+except DuplicateTable:
+    print(f"Table {messages_table_name} already exist.")
+    pass
+
+
+#end of connection
 cursor.close()
 cnx.close()
-
